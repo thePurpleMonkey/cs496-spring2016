d66f1c8c180150fa47706734beb8f667bcc84b10
Peter Kessinger (Windows Desktop) <pkessinger8@gmail.com>
2016-04-18 21:01:13 -0700

    Added settings and added Icon in actionbar

--- BestSlopes2/app/src/main/java/com/best_slopes/bestslopes/MainActivity.java
+++ BestSlopes2/app/src/main/java/com/best_slopes/bestslopes/MainActivity.java
@@ -1,10 +1,16 @@
 package com.best_slopes.bestslopes;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_DIFFICULTY = "extra hard";
@@ -14,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Allow icon to be displayed in ActionBar
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
     }
 
     @Override
@@ -22,6 +33,32 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_file, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Context context = getApplicationContext();
+        CharSequence text = "Currently the default settings layout";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+
+        switch (item.getItemId()) {
+
+            case R.id.menu_add_run:
+
+                return true;
+
+            case R.id.menu_settings:
+                final Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                toast.show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     public void startActivity(View view) {
         Intent intent = new Intent(this, EditTrail.class);

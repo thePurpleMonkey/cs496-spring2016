c444550321318895403ba78b4d1148eda7a32350
Muratcan Çiçek <muratcancicek0@gmail.com>
2016-04-25 18:59:40 -0700

    Add comments field is updated

--- BestSlopes2/app/src/main/java/com/best_slopes/bestslopes/ImageAdapter.java
+++ BestSlopes2/app/src/main/java/com/best_slopes/bestslopes/ImageAdapter.java
@@ -9,7 +9,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -100,13 +106,34 @@ public class ImageAdapter extends BaseAdapter {
                 if(f.exists()) {
                     BitmapFactory.Options bmOptions = new BitmapFactory.Options();
                     result = BitmapFactory.decodeFile(f.getAbsolutePath(), bmOptions);
+                    result = getRoundedCornerBitmap(result, 34);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return result;
         }
+        private Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
+            Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap
+                    .getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(output);
 
+            final int color = 0xff424242;
+            final Paint paint = new Paint();
+            final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+            final RectF rectF = new RectF(rect);
+            final float roundPx = pixels;
+
+            paint.setAntiAlias(true);
+            canvas.drawARGB(0, 0, 0, 0);
+            paint.setColor(color);
+            canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            canvas.drawBitmap(bitmap, rect, rect, paint);
+
+            return output;
+        }
         private void setOnClickListener() {
             this.imageView.setOnClickListener(new View.OnClickListener() {
                 //@Override

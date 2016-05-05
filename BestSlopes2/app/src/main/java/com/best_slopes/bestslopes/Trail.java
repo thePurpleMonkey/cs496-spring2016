3dc30a25d79b98a7b0f19041f5ba341c269442c5
Michael Humphrey <mhumphree@gmail.com>
2016-05-04 22:53:11 -0700

    imagePaths was not initialized.

--- BestSlopes2/app/src/main/java/com/best_slopes/bestslopes/Trail.java
+++ BestSlopes2/app/src/main/java/com/best_slopes/bestslopes/Trail.java
@@ -18,6 +18,7 @@ public class Trail {
         this.rating = 0.0f;
         this.difficulty = 0;
         this.comments = new ArrayList<String>();
+        this.imagePaths = new ArrayList<String>();
         this.id = -1;
     }
 
@@ -26,6 +27,7 @@ public class Trail {
         this.rating = rating;
         this.difficulty = difficulty;
         this.comments = new ArrayList<String>();
+        this.imagePaths = new ArrayList<String>();
         this.id = -1;
     }
 
@@ -34,6 +36,7 @@ public class Trail {
         this.rating = rating;
         this.difficulty = difficulty;
         this.comments = new ArrayList<String>();
+        this.imagePaths = new ArrayList<String>();
         this.id = -1;
     }
 
@@ -42,6 +45,7 @@ public class Trail {
         this.rating = rating;
         this.difficulty = difficulty;
         this.comments = new ArrayList<String>();
+        this.imagePaths = new ArrayList<String>();
         this.id = id;
 
         for (String comment : comments) {
@@ -107,9 +111,7 @@ public class Trail {
         }
     }
 
-    public ArrayList<String> getImagePaths() {  // Jhon: It should be filled from database // This is just for debug
-        imagePaths = new ArrayList<String>();
-
+    public ArrayList<String> getImagePaths() {
         return imagePaths;
     }
 

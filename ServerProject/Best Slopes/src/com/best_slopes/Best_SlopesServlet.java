eb189cf111c62e1760fb5d8c2911f465fad87f41
Peter Kessinger (Windows Desktop) <pkessinger8@gmail.com>
2016-05-16 10:30:56 -0700

    Added eclipse ServerProject director

---
+++ ServerProject/Best Slopes/src/com/best_slopes/Best_SlopesServlet.java
@@ -0,0 +1,12 @@
+package com.best_slopes;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Best_SlopesServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}

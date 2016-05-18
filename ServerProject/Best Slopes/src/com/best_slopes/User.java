c3c8fac681f6d7aa0d0f99da6d4ef5a5658e6a89
Peter Kessinger (Windows Desktop) <pkessinger8@gmail.com>
2016-05-17 22:44:14 -0700

    Added User.java

---
+++ ServerProject/Best Slopes/src/com/best_slopes/User.java
@@ -0,0 +1,17 @@
+package com.best_slopes;
+
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Text;
+
+@PersistenceCapable
+public class User {
+	
+	
+}

aca9a08b1e20b79147f1957e1e5517b647267fb0
Muratcan Çiçek <muratcancicek0@gmail.com>
2016-06-06 19:17:23 -0700

    Server Statistics

--- BestSlopes2/app/src/main/java/com/best_slopes/bestslopes/ServerStats.java
+++ BestSlopes2/app/src/main/java/com/best_slopes/bestslopes/ServerStats.java
@@ -1,38 +1,17 @@
 package com.best_slopes.bestslopes;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.List;
-
 public class ServerStats extends Activity {
-    TextView content;
-    List<String> serverData;
     private Stats stats = new Stats();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_stats);
-
-        LinearLayout myLayout = (LinearLayout) findViewById(R.id.server_stats_layout);
-        TextView text = new TextView((getApplicationContext()));
-        text.setTextColor(Color.BLUE);
-
-        /** Set title **/
-        text.setText("Server Statistics");
-        text.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        myLayout.addView(text);
-
 
         /** Get stats from server **/
         ServerComms.LoadStatsFromServer getStats = new ServerComms.LoadStatsFromServer();
@@ -43,37 +22,16 @@ public class ServerStats extends Activity {
         }
 
         /** Load stats into text view **/
-        //TODO: Update this with real data
-        text = new TextView(getApplicationContext());
-        text.setTextColor(Color.RED);
-        text.setText("Active trails in past Hour " + stats.getHourCount());
-        text.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        myLayout.addView(text);
+        TextView hourCount = (TextView) findViewById(R.id.hourCountText);
+        hourCount.setText(String.valueOf(stats.getHourCount()));
 
-        text = new TextView(getApplicationContext());
-        text.setTextColor(Color.RED);
-        text.setText("Active trails in past Day " + stats.getDayCount());
-        text.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        myLayout.addView(text);
+        TextView dayCount = (TextView) findViewById(R.id.dayCountText);
+        dayCount.setText(String.valueOf(stats.getDayCount()));
 
-        text = new TextView(getApplicationContext());
-        text.setTextColor(Color.RED);
-        text.setText("Active trails in past Week " + stats.getWeekCount());
-        text.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        myLayout.addView(text);
+        TextView weekCount = (TextView) findViewById(R.id.weekCountText);
+        weekCount.setText(String.valueOf(stats.getWeekCount()));
 
-        text = new TextView(getApplicationContext());
-        text.setTextColor(Color.RED);
-        text.setText("Active trails in Total " + stats.getTotalCount());
-        text.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        myLayout.addView(text);
+        TextView totalCount = (TextView) findViewById(R.id.totalCountText);
+        totalCount.setText(String.valueOf(stats.getTotalCount()));
     }
 }
